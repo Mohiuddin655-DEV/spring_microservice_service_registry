332e7140ae74ce00881b45830bbca13cc1cd72ba
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-04-30 10:43:55 +0600

    v1.0.0

---
+++ src/test/java/com/example/spring_service_registry/SpringServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
